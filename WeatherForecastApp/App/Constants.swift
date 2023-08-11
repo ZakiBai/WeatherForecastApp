c79caffb6c0f87f7d06aab4607068cabfff5fa14
Zaki <menza.zm@gmail.com>
2023-08-11 09:26:26 +0600

    Alert Controller is done

---
+++ WeatherForecastApp/App/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  WeatherForecastApp
+//
+//  Created by Zaki on 10.08.2023.
+//
+
+import Foundation
---
+++ WeatherForecastApp/Services/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  WeatherForecastApp
+//
+//  Created by Zaki on 10.08.2023.
+//
+
+import Foundation
